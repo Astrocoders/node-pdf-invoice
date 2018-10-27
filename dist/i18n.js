43b14abc09ee4034d16be9f911162194ab2fca75
Nicola Gheza <ghzncl@gmail.com>
2018-10-27 14:21:42 +0200

    Update i18n.js

--- dist/i18n.js
+++ dist/i18n.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   en_US: {},
-  pt_BR: {
-    createdAt: 'Criado em',
-    amount: 'Total',
-    chargeStatus: 'Status de cobrança',
-    chargeFor: 'Cobrança para: ',
-    description: 'Descrição',
+  it_IT: {
+    createdAt: 'Creato il',
+    amount: 'Totale',
+    chargeStatus: 'Stato di fatturazione',
+    chargeFor: 'Fatturazione per: ',
+    description: 'Descrizione',
     name: 'Nome',
-    quantity: 'Quantidade'
-  }
-};
+    quantity: 'Quantità',
+  },
+}
