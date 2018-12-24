e4a294b2494617134185fc70dec18d4686db73ad
Yegor Kudinov <bp.headwire@gmail.com>
2018-12-24 12:25:39 +0200

    update PDF formatting

--- dist/index.js
+++ dist/index.js
@@ -1,55 +1,64 @@
-'use strict';
+const pdfKit = require('pdfkit');
+const moment = require('moment');
+const numeral = require('numeral');
+const i18n = require('./i18n');
 
-var pdfKit = require('pdfkit');
-var moment = require('moment');
-var numeral = require('numeral');
-var i18n = require('./i18n');
+const TEXT_SIZE = 8;
+const CONTENT_LEFT_PADDING = 50;
 
-var TEXT_SIZE = 8;
-var CONTENT_LEFT_PADDING = 50;
-
-function PDFInvoice(_ref) {
-  var company = _ref.company;
-  var customer = _ref.customer;
-  var items = _ref.items;
-
-  var date = new Date();
-  var charge = {
-    createdAt: date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear(),
-    amount: items.reduce(function (acc, item) {
-      return acc + item.amount;
-    }, 0)
+function PDFInvoice({
+  company, // {phone, email, address}
+  customer, // {name, email, phone, address}
+  items, // [{amount, name, description}]
+  invoiceDate,
+}){
+  const date = new Date(invoiceDate);
+  const charge = {
+    createdAt: `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`,
+    amount: items.reduce((acc, item) => acc + item.amount, 0),
   };
-  var doc = new pdfKit({ size: 'A4', margin: 50 });
+  const doc = new pdfKit({size: 'A4', margin: 50});
 
   doc.fillColor('#333333');
 
-  var translate = i18n[PDFInvoice.lang];
+  const translate = i18n[PDFInvoice.lang];
   moment.locale(PDFInvoice.lang);
 
-  var divMaxWidth = 550;
-  var table = {
+  const divMaxWidth = 550;
+  const table = {
     x: CONTENT_LEFT_PADDING,
     y: 200,
-    inc: 150
+    inc: 150,
   };
 
   return {
-    genHeader: function genHeader() {
-      doc.fontSize(20).text(company.name, CONTENT_LEFT_PADDING, 50);
+    genHeader(){
+      doc
+       .fontSize(20)
+       .text(company.name, CONTENT_LEFT_PADDING, 50);
 
-      var borderOffset = doc.currentLineHeight() + 70;
+      const borderOffset = doc.currentLineHeight() + 70;
 
-      doc.fontSize(16).fillColor('#cccccc').text(moment().format('MMMM, DD, YYYY'), CONTENT_LEFT_PADDING, 50, {
-        align: 'right'
-      }).fillColor('#333333');
+      doc
+        .fontSize(16)
+        .fillColor('#cccccc')
+        .text(moment(date).format('MMMM, DD, YYYY'), CONTENT_LEFT_PADDING, 50, {
+          align: 'right',
+        })
+        .fillColor('#333333');
 
-      doc.strokeColor('#cccccc').moveTo(CONTENT_LEFT_PADDING, borderOffset).lineTo(divMaxWidth, borderOffset);
+      doc
+        .strokeColor('#cccccc')
+        .moveTo(CONTENT_LEFT_PADDING, borderOffset)
+        .lineTo(divMaxWidth, borderOffset);
     },
-    genFooter: function genFooter() {
+
+    genFooter(){
       doc.fillColor('#cccccc');
 
-      doc.fontSize(12).text(company.name, CONTENT_LEFT_PADDING, 450);
+      doc
+        .fontSize(12)
+        .text(company.name, CONTENT_LEFT_PADDING, 500);
 
       doc.text(company.address);
       doc.text(company.phone);
@@ -57,32 +66,59 @@ function PDFInvoice(_ref) {
 
       doc.fillColor('#333333');
     },
-    genCustomerInfos: function genCustomerInfos() {
-      doc.fontSize(TEXT_SIZE).text(translate.chargeFor, CONTENT_LEFT_PADDING, 400);
 
-      doc.text(customer.name + ' <' + customer.email + '>');
+    genCustomerInfos(){
+      doc
+        .fontSize(TEXT_SIZE)
+        .text(translate.chargeFor, CONTENT_LEFT_PADDING, 400);
+
+      doc.text(`${customer.name}`);
+      doc.text(`${customer.address}`);
+      doc.text(`${customer.phone}`);
+      doc.text(`${customer.email}`);
     },
-    genTableHeaders: function genTableHeaders() {
-      ['amount', 'name', 'description', 'quantity'].forEach(function (text, i) {
-        doc.fontSize(TEXT_SIZE).text(translate[text], table.x + i * table.inc, table.y);
+
+    genTableHeaders(){
+      [
+        'amount',
+        'name',
+        'description',
+        'quantity',
+      ].forEach((text, i) => {
+        doc
+          .fontSize(TEXT_SIZE)
+          .text(translate[text], table.x + i * table.inc, table.y);
       });
     },
-    genTableRow: function genTableRow() {
-      items.map(function (item) {
-        return Object.assign({}, item, {
-          amount: numeral(item.amount).format('$ 0,00.00')
+
+    genTableRow(){
+      items
+        .map(item => Object.assign({}, item, {
+          amount: '€' + numeral(item.amount)
+        }))
+        .forEach((item, itemIndex) => {
+          [
+            'amount',
+            'name',
+            'description',
+            'quantity',
+          ].forEach((field, i) => {
+            doc
+              .fontSize(TEXT_SIZE)
+              .text(item[field], table.x + i * table.inc, table.y + TEXT_SIZE + 6 + itemIndex * 20);
+          });
         });
-      }).forEach(function (item, itemIndex) {
-        ['amount', 'name', 'description', 'quantity'].forEach(function (field, i) {
-          doc.fontSize(TEXT_SIZE).text(item[field], table.x + i * table.inc, table.y + TEXT_SIZE + 6 + itemIndex * 20);
-        });
-      });
     },
-    genTableLines: function genTableLines() {
-      var offset = doc.currentLineHeight() + 2;
-      doc.moveTo(table.x, table.y + offset).lineTo(divMaxWidth, table.y + offset).stroke();
+
+    genTableLines(){
+      const offset = doc.currentLineHeight() + 2;
+      doc
+        .moveTo(table.x, table.y + offset)
+        .lineTo(divMaxWidth, table.y + offset)
+        .stroke();
     },
-    generate: function generate() {
+
+    generate(){
       this.genHeader();
       this.genTableHeaders();
       this.genTableLines();
@@ -93,13 +129,12 @@ function PDFInvoice(_ref) {
       doc.end();
     },
 
-
-    get pdfkitDoc() {
+    get pdfkitDoc(){
       return doc;
-    }
+    },
   };
 }
 
-PDFInvoice.lang = 'pt_BR';
+PDFInvoice.lang = 'en_US';
 
 module.exports = PDFInvoice;
